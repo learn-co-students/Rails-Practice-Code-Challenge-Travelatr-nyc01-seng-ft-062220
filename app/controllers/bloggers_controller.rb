6aa810997523a51cd6a1bc48f2a27fb3ac411910
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-22 20:41:39 -0400

    Done.

---
+++ app/controllers/bloggers_controller.rb
@@ -0,0 +1,27 @@
+class BloggersController < ApplicationController
+    def show
+        @blogger = Blogger.find(params[:id])
+        @most_liked_post = @blogger.most_likes
+    end
+
+    def new
+        @blogger = Blogger.new
+    end
+
+    def create
+        @blogger = Blogger.create(blogger_params)
+        if @blogger.valid?
+            redirect_to blogger_path(@blogger)
+        else
+            flash[:my_errors] = @blogger.errors.full_messages
+            redirect_to new_blogger_path
+        end
+    end
+
+
+    private
+
+    def blogger_params
+        params.require(:blogger).permit(:name, :age, :bio)
+    end
+end
--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,30 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+
+    validates :name, uniqueness: true
+    validates :age, numericality: {greater_than: 0}
+    validates :bio, length: {minimum: 31}
+
+
+    def total_likes
+        total_likes = 0
+        self.posts.each do |post|
+            total_likes += post.likes
+        end
+        total_likes
+    end
+
+    def most_likes
+        @most_liked_post = nil
+        @most_likes = -1
+        self.posts.each do |post|
+            #byebug
+            if post.likes > @most_likes
+                @most_liked_post = post
+            end
+        end
+        @most_liked_post
+    end
 
 end
